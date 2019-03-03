f43e6cdb975ef80631cdef3f3e81a36912fc6ad0
Fabrizio Castellarin <f.castellarin@gmail.com>
2019-03-03 09:27:43 +0100

    Remove the App component css from the general stylesheet

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import { css } from 'glamor'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Synth from './components/Synth'
 import './styles.css'
 
+const app = css({
+  fontFamily: "'Courier New', Courier, monospace",
+  fontSize: '15px',
+  '& button': {
+    fontFamily: "'Courier New', Courier, monospace",
+    height: '20px',
+    lineHeight: '1.5em',
+  },
+})
+
 function App() {
   return (
-    <div className="App">
+    <div {...app}>
       <Synth />
     </div>
   )
